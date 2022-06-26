e9ecb5346e1d8c3a3cf2fbcc462ce306c4b99bbb
Abdul Wahhab <88076391+wahhabc23@users.noreply.github.com>
2022-06-26 09:10:08 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+from .hangman_words import word_list
+import hangman_art
+
+
+word = random.choice(word_list)
+length = len(word)
+
+logo = hangman_art.logo
+print(logo)
+
+dash_word = []
+for n in range(0,length):
+    dash_word.append("_")
+print(dash_word)
+    
+array_word = []
+for n in range(0,length):
+    array_word.append(word[n])
+
+game_over = False
+
+life = 6
+stages = hangman_art.stages
+print(stages[life])
+while not game_over:
+    user_letter = input("Enter a letter ").lower()
+    if (user_letter in array_word):        
+        for n in range(0,length):
+            if user_letter == array_word[n]:
+                dash_word[n] = array_word[n]
+        print(dash_word)
+    else:
+        life -= 1
+        print(stages[life])    
+    
+    if "_" not in dash_word:
+        game_over = True
+        print("You Win")
+    elif life <= 0:
+        game_over = True
+        print("You Lose")    
+print(f"\t\t\t{array_word}")
+input("Press any button to exit....")
+
+
+
